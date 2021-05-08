493e71bf547b324393a53de6d7f4a845582c8373
Daniel Okello <handsdani@gmail.com>
2021-05-08 15:31:20 +0300

    Comparisons in Javascript

--- FreeCodeCamp/javascript.js
+++ FreeCodeCamp/javascript.js
@@ -26,3 +26,41 @@
 
 // That's where .shift() comes in. It works just like .pop(), except it removes the first element instead of the last.
 
+// Not only can you shift elements off of the beginning of an array, you can also unshift elements to the beginning of an array i.e. add elements in front of the array.
+
+// .unshift() works exactly like .push(), but instead of adding the element at the end of the array, unshift() adds the element at the beginning of the array.
+
+
+
+
+
+// Parameters vs Arguments
+
+// Parameters are variables that act as placeholders 
+// for the values that are to be input to a function when it is called. 
+// When a function is defined, it is typically defined along with one or more parameters. 
+
+// Argument=========>The actual values that are input (or "passed") into a function when it is called are known as arguments.
+
+// Parameters passed in a function
+
+function testFun(param1, param2) {
+    console.log(param1, param2);
+  }
+
+//   Arguments
+  testFun('hello','world!');
+
+//   Local Scope and Functions
+
+//   Variables which are declared within a function, 
+//   as well as the function parameters, have local scope. 
+//   That means they are only visible within that function.
+
+/*
+Global vs. Local Scope in Functions
+
+It is possible to have both local and global variables 
+with the same name. When you do this, the local variable 
+takes precedence over the global variable.
+*/
