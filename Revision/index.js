69c7169441e3ebe54326c4da237559b55cea8cd0
Daniel Okello <handsdani@gmail.com>
2021-05-08 11:41:48 +0300

    Primitives and reference types

--- Revision/index.js
+++ Revision/index.js
@@ -6,55 +6,106 @@
         
         // In javascript an object is a collection of key value Pairs
         
-        const circle = {
+        // const circle = {
 
-            // key value pairs
+        //     // key value pairs
 
-            // These are the properties( Hold values)
-            radius: 1,
-            location: {
-                x:1,
-                y:1
-            },
+        //     // These are the properties( Hold values)
+        //     radius: 1,
+        //     location: {
+        //         x:1,
+        //         y:1
+        //     },
 
-            // This is a method (defines some logic)
-            draw: function(){
-                console.log('draw')
-            }
+        //     // This is a method (defines some logic)
+        //     draw: function(){
+        //         console.log('draw')
+        //     }
 
-        };   
+        // };   
 
-        // we'll access these members using the dot notation
+        // // we'll access these members using the dot notation
 
-        circle.draw();
-
-
+        // circle.draw();
 
 
+        // In cases where you want to create a new circle, youd copy and paste the code of the circle,
+        // cloning it. However if there are many methods for a given object this mihgt
+        // seem hectic
+
+        // this makes the object literal syntax difficult in cases of fixing bugs
+        
+        
+    // Behaviour ====> If an object has many methods
 
 
-// Factories and Constructors
+// // Factories and Constructors
 
+//         // Factory
+//         function createCircle(radius){
+//             return{
+//                 radius,
+//                 draw: function(){
+//                     console.log('draw')
+//                 }
+//             };
+//         }
 
+//         const circle = createCircle(1);
+
+//         // Constructors
+//             // We use a constructor function
+            
+//             function Circle(radius){
+
+//                 // the this statement is a reference to the object referencing this code.
+
+//                 this.radius = radius;
+//                 this.draw = function(){
+//                     console.log('draw');
+//                 }
+
+//             }
+
+//             const anotherCircle = new Circle(1);
+
+//             // Every object has a constructor property that references the function that was used to create an object.
+
+    function Circle(radius){
+        this.radius = radius;
+        this.draw = function(){
+            console.log('draw');
+        };
+    }
+    Circle.call({},1);
+    const anotherCircle = new Circle(1);
 
 
 
 // Primitives and reference Types
 
+    // // value types (primitives)
+    //     // number, string,boolea, symbol, undefined,null
+    // // reference types
+    //     // Object,function,Array
 
 
+    //     // Primitives
+
+    //     let x = {value:10};
+    //     let y = x;
+
+    //     x.value = 20;
+
+    //     // Primitives are copied by value;
+    //     // Objects are copied by their reference;
 
 
-// Working with properties
+    //     let obj = {value:10};
+    //     function Increase (obj){
+    //         obj.value ++;
+    //     }
 
+    //     Increase(obj);
+    //     console.log(obj);
 
-
-
-
-// Private properties
-
-
-
-
-
-// Getters and Setters
