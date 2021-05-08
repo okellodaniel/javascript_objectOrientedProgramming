79dc9c4babda75aded644931d51f1fa6e37443c4
Daniel Okello <handsdani@gmail.com>
2021-05-08 13:08:35 +0300

    Shift and pop functions in Manupulating arrays

--- FreeCodeCamp/javascript.js
+++ FreeCodeCamp/javascript.js
@@ -6,3 +6,23 @@
     // multi-dimensional array. ==> AN array nested within another Array
 
     // Access data inside arrays using indexes
+
+    // To add values to an array we use a push method
+
+        /*e.g
+
+            // Setup
+                var myArray = [["John", 23], ["cat", 2]];
+
+                // Only change code below this line
+
+                myArray.push(['dog',3]);*/
+
+// Another way to change the data in an array is with the .pop() function.
+
+// .pop() is used to pop a value off of the end of an array. We can store this popped off value by assigning it to a variable. In other words, .pop() removes the last element from an array and returns that element.
+
+// pop() always removes the last element of an array. What if you want to remove the first?
+
+// That's where .shift() comes in. It works just like .pop(), except it removes the first element instead of the last.
+
