7644d0f93ac0e6ad96f7e1e97dd875023081037a
Daniel Okello <handsdani@gmail.com>
2021-05-08 12:55:20 +0300

    Few notes from freecodecamp

---
+++ FreeCodeCamp/javascript.js
@@ -0,0 +1,8 @@
+// Bracket Notation ==> A way to get a character at a specific index within a string
+
+// Zero-based indexing ==> Start counting from 0
+
+// Arrays store multiplevalues in one variable
+    // multi-dimensional array. ==> AN array nested within another Array
+
+    // Access data inside arrays using indexes
