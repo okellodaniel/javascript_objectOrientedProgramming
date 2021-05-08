ad122dbe94dd33f8ea8e393c7f3d4406ff0cc997
Daniel Okello <handsdani@gmail.com>
2021-05-08 19:30:47 +0300

    Else if statements

---
+++ FreeCodeCamp/ifelse.js
@@ -0,0 +1,31 @@
+// elseif statements
+function foo(x){
+    if(x<2){
+        console.log('less than 2' ) 
+    }
+    else if (x<1){
+        console.log( 'less than 1') 
+    }
+    else{
+        console.log( 'greater than or equal to 2') 
+    }
+};
+
+function bar(x){
+    if(x<1){
+        console.log( 'less than 1') 
+    }
+    else if (x<2){
+        console.log('less than 2' ) 
+    }
+    else{
+        console.log( 'greater than or equal to 2') 
+    }
+};
+
+foo(0);
+bar(0);
+
+
+
+
