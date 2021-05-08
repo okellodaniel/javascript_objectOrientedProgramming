716f368a3157b6ad7bd712fd1f0795c8547941aa
Daniel Okello <handsdani@gmail.com>
2021-05-08 10:57:56 +0300

    Object literals, Factories and constructors

---
+++ Revision/OOP.js
@@ -0,0 +1,52 @@
+// OOP is a programming paradigm that is centered around objectsrather than functions.
+
+// It is a style of programming.
+
+// OOP principles
+
+//  Encapsulation
+
+    // We group functions that operate on the variables and variables into objects (Encapsulation)
+    //  
+
+//  Abstraction
+    // Hide some of the properties and methods from the outside
+
+    // benefits
+        // Make the interface simpler
+        // reduces impact of change
+
+//  Polymorphism 
+    // Poly==>Many
+    // morph ==> forms
+    // technique that prevents long switch case statements 
+
+//  Inheritance
+
+    // allows you to eliminate redundant code. 
+
+//  Group functions and variables into a unit KNown as an object
+// Functions ==> Methods
+// Variables ==> Property
+
+//the best functions are those without parameters
+
+// Benefits of OOP in JS
+
+    // Encapsulation 
+        // ==> Group related functions and variables together (reduce complexity)
+        // ==> Increase re-usability
+
+    // Abstraction
+        // Hide the details and complexity and show only the essentials
+            // ==>Reduces complexity
+            // ==>Isolates the impact of changes in code
+
+    // Inheritance
+        // We eliminate redundant code
+
+    // Polymorphism
+        // refactor ugly switch/case statements
+
+
+        
---
+++ Revision/index.js
@@ -0,0 +1,60 @@
+// Creating Objects
+
+    // Object Literals
+
+        // curly braces are object literal syntax
+        
+        // In javascript an object is a collection of key value Pairs
+        
+        const circle = {
+
+            // key value pairs
+
+            // These are the properties( Hold values)
+            radius: 1,
+            location: {
+                x:1,
+                y:1
+            },
+
+            // This is a method (defines some logic)
+            draw: function(){
+                console.log('draw')
+            }
+
+        };   
+
+        // we'll access these members using the dot notation
+
+        circle.draw();
+
+
+
+
+
+
+// Factories and Constructors
+
+
+
+
+
+// Primitives and reference Types
+
+
+
+
+
+// Working with properties
+
+
+
+
+
+// Private properties
+
+
+
+
+
+// Getters and Setters
